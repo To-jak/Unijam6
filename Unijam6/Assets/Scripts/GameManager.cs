4ba91d55082722f6f5528f182b1f1092717dac08
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2017-11-18 01:43:12 +0100

    new start & end of level + gamemanager

---
+++ Unijam6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        player.transform.position = GameObject.FindGameObjectWithTag("Start").transform.position;
+    }
+}
