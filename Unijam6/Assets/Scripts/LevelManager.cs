1151c4848f131ea775027350a8aadde417a33e19
@coussemacker <kevin.coussemacker@telecom-sudparis.eu>
2017-11-18 02:42:56 +0100

    Changed LevelManager

--- Unijam6/Assets/Scripts/LevelManager.cs
+++ Unijam6/Assets/Scripts/LevelManager.cs
@@ -21,33 +21,8 @@ public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(int i)
     {
-        switch (i)
-        {
-            case 1:
-                SceneManager.LoadScene("Level1");
-                break;
-            case 2:
-                SceneManager.LoadScene("Level2");
-                break;
-            case 3:
-                SceneManager.LoadScene("Level3");
-                break;
-            case 4:
-                SceneManager.LoadScene("Level4");
-                break;
-            case 5:
-                SceneManager.LoadScene("Level5");
-                break;
-            case 6:
-                SceneManager.LoadScene("Level6");
-                break;
-            case 7:
-                SceneManager.LoadScene("Level7");
-                break;
-            case 8:
-                SceneManager.LoadScene("Level8");
-                break;
-        }
+        string level = "Level" + i.ToString();
+        SceneManager.LoadScene(level);
 
 
     }
